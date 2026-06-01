af7797c1924f9b3a3c677ae157438742d96f72c0
NewLeaner000 <sangww682@gmail.com>
2026-06-01 12:48:52 +0700

    Model trainner done

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 import dill
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 
@@ -18,4 +19,26 @@ def save_object(file_path,obj):
 
     except Exception as e:
         logging.info("Error in saving object")
+        raise CustomException(e,sys)
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(models)):
+            model=models[list(models.keys())[i]]
+            model.fit(X_train,y_train)
+
+            y_train_pred=model.predict(X_train)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.info("Error in evaluating models")
         raise CustomException(e,sys)
